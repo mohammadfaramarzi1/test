23aa96de28acee73d838c995f159e7d88540b9be
Mohammad Faramarzi <faramarzi.dev@gmail.com>
2024-10-27 13:22:57 +0330

    refactor islogin bug on master branch

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 let userAge = 12;
 
 const isLogin = (username) => {
-  if (username === "amin") {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const register = (username) => {
-  if (username) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 };
